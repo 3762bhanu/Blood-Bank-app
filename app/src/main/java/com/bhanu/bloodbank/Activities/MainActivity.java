6ee31699398674b4c77472a641758bf810c4d73b
3762bhanu <lit2019043@iiitl.ac.in>
2021-06-08 13:41:07 +0530

    android application can be used by needy person.

---
+++ app/src/main/java/com/bhanu/bloodbank/Activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.bhanu.bloodbank.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.bhanu.bloodbank.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
